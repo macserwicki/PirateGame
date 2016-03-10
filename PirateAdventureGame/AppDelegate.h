710354f1d701c0a959ecb4d40e058d1ea1258f86
Maciej Serwicki <macserwicki@gmail.com>
2016-03-09 23:45:59 -0600

    Initial Commit

---
+++ PirateAdventureGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PirateAdventureGame
+//
+//  Created by Maciej Serwicki on 3/9/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PirateAdventureGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PirateAdventureGame
+//
+//  Created by Maciej Serwicki on 3/9/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
