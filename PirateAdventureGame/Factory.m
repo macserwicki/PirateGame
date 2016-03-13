004bb14a70b7c85be03ca435ffc190d2089700ce
Maciej Serwicki <macserwicki@gmail.com>
2016-03-13 03:27:01 -0500

    Game Tiles Made, Will Use Them To Make Gameplay Grid

--- PirateAdventureGame/Factory.m
+++ PirateAdventureGame/Factory.m
@@ -55,10 +55,83 @@
     dogArmor.name = @"Dog";
     Weapon *dogWeapon = [[Weapon alloc] init];
     dogWeapon.name = @"Dog Claw";
+    dogWeapon.damage = 10;
+    tile4.armor = dogArmor;
+    tile4.weapon = dogWeapon;
+    tile4.actionBtnName = @"Adobt The Dog";
+    tile4.healthEffect = 0;
     
+    Tile *tile5 = [[Tile alloc] init];
+    tile5.story = @"You've found a chest, opening it reveald a beautiful double-headed axe. Should we take it?";
+    tile5.itemImg = [UIImage imageNamed:@"axe"];
+    Weapon *axeWeapon = [[Weapon alloc] init];
+    axeWeapon.name = @"Axe";
+    axeWeapon.damage = 17;
+    tile5.healthEffect = 0;
+    tile5.weapon = axeWeapon;
+    tile5.actionBtnName = @"Take The Axe";
+    
+    Tile *tile6 = [[Tile alloc] init];
+    tile6.story = @"Oh no! It's a trap, and you've been captured. We have to fight for our life!";
+    tile6.itemImg = [UIImage imageNamed:@"bandit"];
+    tile6.healthEffect = -22;
+    tile6.actionBtnName = @"Fight For Your Life";
+    
+    NSMutableArray *secondColumn = [[NSMutableArray alloc] init];
+    [secondColumn addObject:tile4];
+    [secondColumn addObject:tile5];
+    [secondColumn addObject:tile6];
+
+    Tile *tile7 = [[Tile alloc] init];
+    tile7.story = @"What is that on the horizon? Is that... Is that a walking shark with a laser? Should we attack it?";
+    tile7.itemImg = [UIImage imageNamed:@"shark"];
+    tile7.healthEffect = 8;
+    tile7.actionBtnName = @"Turn it into sushi, and eat it.";
+    
+    Tile *tile8 = [[Tile alloc] init];
+    tile8.story = @"The Wyvern has spotted us and is headed directly toward us. Run and hide!";
+    tile8.itemImg = [UIImage imageNamed:@"wyvern"];
+    tile8.healthEffect = -48;
+    tile8.actionBtnName = @"Run and Hide";
+    
+    
+    Tile *tile9 = [[Tile alloc] init];
+    tile9.story = @"When hiding from the Wyvern you found another treasure chest. You open it to see if it has anything useful and find a good tasting potion";
+    tile9.itemImg = [UIImage imageNamed:@"treasure"];
+    tile9.healthEffect = 20;
+    tile9.actionBtnName = @"Loot The Treasure Chest";
+    
+    NSMutableArray *thirdColumn = [[NSMutableArray alloc] init];
+    [thirdColumn addObject:tile7];
+    [thirdColumn addObject:tile8];
+    [thirdColumn addObject:tile9];
+
+    Tile *tile10 = [[Tile alloc] init];
+    tile10.story = @"Bandits surround you and demand you give them all your findings. Your instincts tell you that these bandtis will kill you anyways once you hand them your weapons, so you decide to stay and fight them because they blocked your path.";
+    tile10.itemImg = [UIImage imageNamed:@"bandit"];
+    tile10.healthEffect = -15;
+    tile10.actionBtnName = @"Show Them Who's Alpha";
+    
+    Tile *tile11 = [[Tile alloc] init];
+    tile11.story = @"You follow the river to a peaceful lake. Looks benign, but you never know what might be behind the thick tree line.";
+    tile11.itemImg = [UIImage imageNamed:@"lake"];
+    tile11.healthEffect = -7;
+    tile11.actionBtnName = @"Swim Lake";
+    
+    Tile *tile12 = [[Tile alloc] init];
+    tile12.story = @"This is it. Your hard work and exploring prepared you to face off the  mutated Mega Turkey-Chicken Boss. It seems a lot less smaller and less scary than how the legends describe it.";
+    tile12.itemImg = [UIImage imageNamed:@"chicken"];
+    tile12.healthEffect = -10;
+    tile12.actionBtnName = @"Fight Boss!";
+    
+    NSMutableArray *fourthColumn = [[NSMutableArray alloc] init];
+    [fourthColumn addObject: tile10];
+    [fourthColumn addObject: tile11];
+    [fourthColumn addObject: tile12];
+
     
     //NSMutableArrays (3 tiles) inside 'tiles' array.
-    NSArray *tiles = [[NSArray alloc] initWithObjects:firstColomn, nil];
+    NSArray *tiles = [[NSArray alloc] initWithObjects:firstColomn, secondColumn, thirdColumn, fourthColumn, nil];
     
     return tiles;
 }
